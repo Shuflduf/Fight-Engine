6ef391501ecf906b7733f3b6defc69276306d3e1
Shuflduf <shuflduf@gmail.com>
2026-01-09 11:01:51 -0700

    grenade

--- Skills/Commando/grenade_projectile.gd
+++ Skills/Commando/grenade_projectile.gd
@@ -1,9 +1,28 @@
 extends RigidBody3D
 
 
-func _on_body_entered(body: Object) -> void:
-	print(body)
-	#if body is CollisionObject3D:
-		#if body.get_collision_layer_value(0):
-	#physics_material_override.bounce = 0.0
+func _on_body_entered(_body: Object) -> void:
 	linear_velocity /= 2.0
+
+
+func _on_explode_timer_timeout() -> void:
+	for body in $ExplosionRadius.get_overlapping_bodies():
+		print(body)
+		var rocket_jump_node = body.get_node(^"RocketJump")
+		if rocket_jump_node:
+			var vec_to_body = body.global_position - global_position
+			var distance = vec_to_body.length()
+			var direction_away = vec_to_body.normalized()
+
+			var splash_radius = 120.0
+			var damage = 5.0 * (1.0 - distance / splash_radius)
+
+			var knockback = min(damage, 200.0)
+
+			rocket_jump_node.jump(direction_away * knockback)
+	for hitbox in $ExplosionRadius.get_overlapping_areas():
+		print(hitbox)
+		if hitbox.name == &"Hitbox":
+			hitbox.hit(10.0)
+
+	queue_free()
--- Skills/Commando/grenade_skill.gd
+++ Skills/Commando/grenade_skill.gd
@@ -13,4 +13,4 @@ func _physics_process(_delta: float) -> void:
 		var new_nade: RigidBody3D = nade_scene.instantiate()
 		add_child(new_nade)
 		new_nade.global_position = cam.global_position
-		new_nade.linear_velocity = -cam.basis.z.normalized() * 15.0
+		new_nade.linear_velocity = player.velocity + (-cam.basis.z.normalized() * 15.0)
